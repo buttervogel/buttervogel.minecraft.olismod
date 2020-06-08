f00d5ac159ae5b080fe936d59d9fa8df820e8b27
oliver.meimberg <oliver.meimberg@form4.de>
2020-06-08 21:41:05 +0200

    Item Funktionen - Tutorial #8

--- src/main/java/buttervogel/minecraft/olismod/item/Bimbam.java
+++ src/main/java/buttervogel/minecraft/olismod/item/Bimbam.java
@@ -1,11 +1,34 @@
 package buttervogel.minecraft.olismod.item;
 
 import buttervogel.minecraft.olismod.init.ItemGroups;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Rarity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 
 public class Bimbam extends Item {
 
     public Bimbam() {
-        super(new Properties().group(ItemGroups.group));
+        super(new Properties().group(ItemGroups.group).defaultMaxDamage(20).rarity(Rarity.UNCOMMON));
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
+        ItemStack stack = player.getHeldItem(hand);
+        if (world.isRemote) {
+            world.setBlockState(player.getPosition().down(), Blocks.DIAMOND_BLOCK.getDefaultState());
+            stack.damageItem(1, player, onBroken -> {
+            });
+        }
+        player.jump();
+        player.fallDistance = 0;
+
+        return new ActionResult<ItemStack>(ActionResultType.SUCCESS, stack);
+
     }
 }
